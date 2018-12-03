31990db817f7888239f58dd36be9a3d43173a4ed
Yangtao Ge <43610450+YangtaoGe518@users.noreply.github.com>
2018-12-03 13:58:56 +0000

    coding style changes

--- Test_omar_New(motor)/motor/motor.ino
+++ Test_omar_New(motor)/motor/motor.ino
@@ -9,24 +9,32 @@ int rps = 0 ;
 /* global variables */
 int last = 0;
 int rpm;
+
 void setup()
 {
   pinMode(motorPin, OUTPUT);
   pinMode(13, OUTPUT);
+  
   digitalWrite(13, HIGH);
-  analogWrite(motorPin,255);
-  while (millis() < 5000) {
+  analogWrite(motorPin, 255);
+  // obtain the sensitive value
+  while (millis() < 5000) 
+  {
     sensorValue = analogRead(rpmPin);
-    if (sensorValue > sensorMax) {
+    if (sensorValue > sensorMax) 
+    {
       sensorMax = sensorValue;
     }
-    if (sensorValue < sensorMin) {
+    if (sensorValue < sensorMin) 
+    {
       sensorMin = sensorValue;
     }
   }
-  analogWrite(motorPin,0);
+  analogWrite(motorPin, 0);
   digitalWrite(13, LOW);
   Serial.begin(9600);
+  
+  // when serial is empty, requires input
   while (! Serial);
   Serial.println("Speed 0 to 255");
 }
@@ -34,11 +42,11 @@ void setup()
 void loop()
 {
   int speed;
-  RPM();
+  rpm = getRpm();
   Serial.println(rpm);
   speed = Serial.parseInt();
   inputSpeed(speed);
-  
+
   delay (5); // delay in between read for stability
 }
 
@@ -54,24 +62,29 @@ void inputSpeed(int speed)
   }
 }
 
-void RPM(){
-unsigned long start_time = millis();
-unsigned long end_time = start_time;
-int count = 0;
-int sensor = analogRead(rpmPin);
-int halfpoint = sensorMax/2 ;
-int newstate = 0 ;
-while(end_time - start_time < 1000){
-  if (sensor < halfpoint && newstate != 1){
-  count++ ;
-  newstate = 1;
+void getRpm() {
+  unsigned long start_time = millis();
+  unsigned long end_time = start_time;
+  int count = 0;
+  int sensor = analogRead(rpmPin);
+  int halfpoint = sensorMax / 2 ;
+  int newstate = 0 ;
+  while (end_time - start_time < 1000)
+  {
+    if (sensor < halfpoint && newstate != 1)
+    {
+      count++ ;
+      newstate = 1;
+    }
+    else if (sensor > halfpoint)
+    {
+      newstate = 0 ;
+    }
+    sensor = analogRead(rpmPin);
+    end_time = millis();
   }
-  else if (sensor > halfpoint) {
-   newstate = 0 ;
-  }
-  sensor = analogRead(rpmPin);
-  end_time = millis();
-}
-rps = count ;
-rpm = rps*60 ; 
+  
+  rps = count ;
+  rpm = rps * 60 ;
+  return rpm
 }
